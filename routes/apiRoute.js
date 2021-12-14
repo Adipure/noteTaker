0be67b7ba1f07b1af5970a3c47fd7490e02f780e
Chanwoo Hwang <Chanwoo1017@hotmail.com>
2021-12-13 23:02:23 -0800

    adding route for delete note and add note

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -2,31 +2,31 @@ const router = require('express').Router()
 const path = require('path')
 const fs = require('fs')
 
-router.get('/add', (req, res) => {
+router.get('/api/notes', (req, res) => {
  fs.readFile(path.join(__dirname, '..', 'data', 'db.json'), 'utf8', (err, data) => {
   if (err) { console.log(err) }
   res.json(JSON.parse(data))
  })
 })
 
-router.post('/items', (req, res) => {
- const item = req.body
+router.post('/api/notes', (req, res) => {
+ const addNote = req.body
  fs.readFile(path.join(__dirname, '..', 'data', 'db.json'), 'utf8', (err, data) => {
   if (err) { console.log(err) }
-  const items = JSON.parse(data)
-  items.push(item)
-  //writeFile can only write a 'string' so we need to convert it to 'string'
-  fs.writeFile(path.join(__dirname, '..', 'data', 'db.json'), JSON.stringify(items), err => {
+  const addmoreNote = JSON.parse(data)
+  addmoreNote.push(addNote)
+  fs.writeFile(path.join(__dirname, '..', 'data', 'db.json'), JSON.stringify(addmoreNote), err => {
    if (err) { console.log(err) }
    res.sendStatus(200)
   })
  })
 })
 
-router.post('/remove', (req, res) => {
- const items = req.body
- console.log(req.body)
- fs.writeFile(path.join(__dirname, '..', 'data', 'db.json'), JSON.stringify(items), err => {
+router.delete('/api/notes/:id', (req, res) => {
+ const addNote = req.body
+ 
+ const deleteNote = JSON.parse(data)
+ fs.writeFile(path.join(__dirname, '..', 'data', 'db.json'), JSON.stringify(deleteNote), err => {
   if (err) { console.log(err) }
   res.sendStatus(200)
  })
--- server.js
+++ server.js
@@ -3,11 +3,9 @@ const path = require ('path')
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'public')))
-//when we are getting a json, (urlencoded) helps to recieve complex json data
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.use(require('./routes/apiRoutes.js'))
 app.use(require('./routes/htmlRoutes.js'))
-
 app.listen(3000)
